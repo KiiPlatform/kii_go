0811ceb035cd17842ab459934a57c32a7789ff3a
satoshi <satoshi.kumano@kii.com>
2015-11-18 12:50:17 +0900

    Format code.

--- kii_cloud_api.go
+++ kii_cloud_api.go
@@ -1,126 +1,126 @@
 package kii
 
 import (
-        "bytes"
-        "encoding/json"
-        "fmt"
-        "io/ioutil"
-        "net/http"
-        "strings"
-        "log"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
 
 type App struct {
-        AppID string
-        AppKey string
-        AppLocation string
+	AppID       string
+	AppKey      string
+	AppLocation string
 }
 
 func (ka *App) HostName() string {
-       lowerLoc := strings.ToLower(ka.AppLocation)
-       switch lowerLoc {
-       case "jp":
-               return "api-jp.kii.com"
-       case "us":
-               return "api.kii.com"
-       case "cn":
-               return "api-cn3.kii.com"
-       case "sg":
-               return "api-sg.kii.com"
-       default:
-               return lowerLoc
-       }
+	lowerLoc := strings.ToLower(ka.AppLocation)
+	switch lowerLoc {
+	case "jp":
+		return "api-jp.kii.com"
+	case "us":
+		return "api.kii.com"
+	case "cn":
+		return "api-cn3.kii.com"
+	case "sg":
+		return "api-sg.kii.com"
+	default:
+		return lowerLoc
+	}
 }
 
 func (ka *App) ThingIFBaseUrl() string {
-        return fmt.Sprintf("https://%s/thing-if/apps/%s", ka.HostName(), ka.AppID)
+	return fmt.Sprintf("https://%s/thing-if/apps/%s", ka.HostName(), ka.AppID)
 }
 
 func (ka *App) KiiCloudBaseUrl() string {
-        return fmt.Sprintf("https://%s/api/apps/%s", ka.HostName(), ka.AppID)
+	return fmt.Sprintf("https://%s/api/apps/%s", ka.HostName(), ka.AppID)
 }
 
 type OnboardGatewayRequest struct {
-        VendorThingID string `json:"vendorThingID"`
-        ThingPassword string `json:"thingPasssword"`
-        ThingType string `json:"thingType"`
-        LayoutPosition string `json:"layoutPosition"`
-        ThingProperties map[string]interface{} `json:"thingProperties"`
+	VendorThingID   string                 `json:"vendorThingID"`
+	ThingPassword   string                 `json:"thingPasssword"`
+	ThingType       string                 `json:"thingType"`
+	LayoutPosition  string                 `json:"layoutPosition"`
+	ThingProperties map[string]interface{} `json:"thingProperties"`
 }
 
 type OnboardGatewayResponse struct {
-        ThingID string `json:"thingID"`
-        AccessToken string `json:"accessToken"`
-        MqttEndPoint MqttEndPoint `json:"mqttEndpooint"`
+	ThingID      string       `json:"thingID"`
+	AccessToken  string       `json:"accessToken"`
+	MqttEndPoint MqttEndPoint `json:"mqttEndpooint"`
 }
 
 type MqttEndPoint struct {
-        InstallationID string `json:"installationID"`
-        Host string `json:"host"`
-        MqttTopic string `json:"mqttTopic"`
-        Username string `json:"userName"`
-        Password string `json:"password"`
-        PortSSL int `json:"portSSL"`
-        PortTCP int `json:"portTCP"`
+	InstallationID string `json:"installationID"`
+	Host           string `json:"host"`
+	MqttTopic      string `json:"mqttTopic"`
+	Username       string `json:"userName"`
+	Password       string `json:"password"`
+	PortSSL        int    `json:"portSSL"`
+	PortTCP        int    `json:"portTCP"`
 }
 
 type APIAuthor struct {
-        Token string
-        App App
+	Token string
+	App   App
 }
 
 func (au *APIAuthor) AnonymousLogin() error {
-        type AnonymousLoginRequest struct {
-                ClientID string `json:"client_id"`
-                ClientSecret string `json:"client_secret"`
-                GrantType string `json:"grant_type"`
-        }
-        type AnonymousLoginResponse struct {
-                ID string `json:"id"`
-                Access_token string `json:"access_token"`
-                Expires_in int `json:"expires_in"`
-                Token_type string `json:"token_type"`
-        }
-        reqObj := AnonymousLoginRequest {
-                ClientID: au.App.AppID,
-                ClientSecret: au.App.AppKey,
-                GrantType: "client_credentials",
-        }
-        reqJson, err := json.Marshal(reqObj)
-        if err != nil {
-                return err
-        }
-        url := fmt.Sprintf("%s/oauth2/token", au.App.KiiCloudBaseUrl())
-        req, err2 := http.NewRequest("POST", url, bytes.NewBuffer(reqJson))
-        if err2 != nil {
-                return err2
-        }
-        req.Header.Set("content-type", "application/json")
+	type AnonymousLoginRequest struct {
+		ClientID     string `json:"client_id"`
+		ClientSecret string `json:"client_secret"`
+		GrantType    string `json:"grant_type"`
+	}
+	type AnonymousLoginResponse struct {
+		ID           string `json:"id"`
+		Access_token string `json:"access_token"`
+		Expires_in   int    `json:"expires_in"`
+		Token_type   string `json:"token_type"`
+	}
+	reqObj := AnonymousLoginRequest{
+		ClientID:     au.App.AppID,
+		ClientSecret: au.App.AppKey,
+		GrantType:    "client_credentials",
+	}
+	reqJson, err := json.Marshal(reqObj)
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("%s/oauth2/token", au.App.KiiCloudBaseUrl())
+	req, err2 := http.NewRequest("POST", url, bytes.NewBuffer(reqJson))
+	if err2 != nil {
+		return err2
+	}
+	req.Header.Set("content-type", "application/json")
 
-        client := &http.Client{}
-        resp, err3 := client.Do(req)
-        if err3 != nil {
-                return err3
-        }
-        defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err3 := client.Do(req)
+	if err3 != nil {
+		return err3
+	}
+	defer resp.Body.Close()
 
-        bodyStr, err4 := ioutil.ReadAll(resp.Body)
-        if err4 != nil {
-                return err4
-        }
-        log.Println("body: " + string(bodyStr))
+	bodyStr, err4 := ioutil.ReadAll(resp.Body)
+	if err4 != nil {
+		return err4
+	}
+	log.Println("body: " + string(bodyStr))
 
-        var respObj AnonymousLoginResponse
-        err5 := json.Unmarshal(bodyStr, &respObj)
-        if err5 != nil {
-                return err5
-        }
-        au.Token = respObj.Access_token
-        return nil
+	var respObj AnonymousLoginResponse
+	err5 := json.Unmarshal(bodyStr, &respObj)
+	if err5 != nil {
+		return err5
+	}
+	au.Token = respObj.Access_token
+	return nil
 }
 
 func (au *APIAuthor) Onboard(request *OnboardGatewayRequest) (OnboardGatewayResponse, error) {
-        // TODO: implement it.
-        var ret OnboardGatewayResponse
-        return ret, nil
+	// TODO: implement it.
+	var ret OnboardGatewayResponse
+	return ret, nil
 }
--- kii_cloud_api_test.go
+++ kii_cloud_api_test.go
@@ -1,46 +1,46 @@
 package kii_test
 
 import (
-        "testing"
-        kii "github.com/KiiPlatform/kii_go"
-        "encoding/json"
+	"encoding/json"
+	kii "github.com/KiiPlatform/kii_go"
+	"testing"
 )
 
 func TestAnonymousLogin(t *testing.T) {
-        app := kii.App {
-                AppID: "9ab34d8b",
-                AppKey: "7a950d78956ed39f3b0815f0f001b43b",
-                AppLocation: "JP",
-        }
-        author := kii.APIAuthor {
-                App: app,
-        }
-        err := author.AnonymousLogin()
-        if err != nil {
-                t.Errorf("got error on anonymous login %s", err)
-        }
-        if len(author.Token) < 1 {
-                t.Errorf("failed to get author token %s", author.Token)
-        }
+	app := kii.App{
+		AppID:       "9ab34d8b",
+		AppKey:      "7a950d78956ed39f3b0815f0f001b43b",
+		AppLocation: "JP",
+	}
+	author := kii.APIAuthor{
+		App: app,
+	}
+	err := author.AnonymousLogin()
+	if err != nil {
+		t.Errorf("got error on anonymous login %s", err)
+	}
+	if len(author.Token) < 1 {
+		t.Errorf("failed to get author token %s", author.Token)
+	}
 }
 
 func TestMarshal(t *testing.T) {
-        requestObj := kii.OnboardGatewayRequest {
-                VendorThingID: "dummyID",
-                ThingPassword: "dummyPass",
-                ThingType: "dummyType",
-                LayoutPosition: "GATEWAY",
-                ThingProperties: map[string]interface{}{
-                        "myCustomString" : "str",
-                        "myNumber" : 1,
-                        "myObject" : map[string]interface{}{
-                                "a" : "b",
-                        },
-                },
-        }
-        str, err := json.Marshal(requestObj)
-        if err != nil {
-                t.Error("failed to marshal object")
-        }
-        t.Logf(string(str))
+	requestObj := kii.OnboardGatewayRequest{
+		VendorThingID:  "dummyID",
+		ThingPassword:  "dummyPass",
+		ThingType:      "dummyType",
+		LayoutPosition: "GATEWAY",
+		ThingProperties: map[string]interface{}{
+			"myCustomString": "str",
+			"myNumber":       1,
+			"myObject": map[string]interface{}{
+				"a": "b",
+			},
+		},
+	}
+	str, err := json.Marshal(requestObj)
+	if err != nil {
+		t.Error("failed to marshal object")
+	}
+	t.Logf(string(str))
 }
